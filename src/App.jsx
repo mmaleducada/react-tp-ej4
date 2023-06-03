03be6c841ba6dbaac69cd6c740770e7c5f465e6b
Malena de Arriba <dearribamalena@gmail.com>
2023-06-03 18:56:16 -0300

    agregue los componentes FormularioTarea, ItemTarea y ListaTarea con sus respectivos maquetados

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import '../src/App.css'
+import { Container } from 'react-bootstrap';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+import FormularioTarea from './components/FormularioTarea';
 
 function App() {
   
   return (
     <>
     <Container className='my-5 main'>
-      <h1 className='display-4 text-center'>Lista de tareas</h1>
-      <hr />
+    <h1 className='display-4 text-center'>Lista de tareas</h1>
+    <hr/>
+    <FormularioTarea></FormularioTarea>
     </Container>
-    <footer className='text-center bg-dark text-light p-4'>
+    <footer className='text-center bg-dark text-light py-4'>
       <p>&copy; Todos los derechos reservados</p>
     </footer>
     </>
