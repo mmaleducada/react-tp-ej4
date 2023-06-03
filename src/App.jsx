203bcdd2ea5d8ec2bf3d0975e2124acbaf618d55
Malena de Arriba <dearribamalena@gmail.com>
2023-06-03 18:43:39 -0300

    se agrega el maquetado inicial que contiene el titulo y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import '../src/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   
   return (
     <>
-      
+    <Container className='my-5 main'>
+      <h1 className='display-4 text-center'>Lista de tareas</h1>
+      <hr />
+    </Container>
+    <footer className='text-center bg-dark text-light p-4'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
