4189fb7d7cb95071a39b597acbbbb4a47a52a5be
Malena de Arriba <dearribamalena@gmail.com>
2023-06-06 20:59:15 -0300

    se agrega la funcion para detener el formulario cuando se produzca el evento submit, el array donde se guardan las tareas y la logica necesaria para obtener el valor del imput, guardarlo en un array y que se renderice cada vez que se crea una nueva tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,22 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+    const [tarea, setTarea] = useState("");
+    const [tareas, setTareas] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTareas([...tareas, tarea])
+    }
+
     return (
         <>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3 d-flex" controlId="tarea">
-                    <Form.Control type="text" placeholder="ingrese una tarea" />
-                    <Button variant="info">Agregar</Button>
+                    <Form.Control type="text"placeholder="ingrese una tarea" onChange={(e) => setTarea(e.target.value)} value={tarea} />
+                    <Button variant="info" type="submit">Agregar</Button>
                 </Form.Group>
             </Form>
             <ListaTareas></ListaTareas>
